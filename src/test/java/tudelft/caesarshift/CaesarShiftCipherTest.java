ca8f104d03d60bb79188483f8bb17e3ab991b3cc
Yosef Ben Yehuda <48494974+YoderBy@users.noreply.github.com>
2022-11-10 10:59:43 +0200

    ghappy exersice

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,15 @@
 package tudelft.caesarshift;
 
 public class CaesarShiftCipherTest {
-
+    public void checkHappy(string str){
+        for (int i =0; i<str.length; i++){
+            if (str.charAt(i) == 'g'){
+                if str.charAt(i+1) !='g'{
+                    return false;
+                }
+                i = i+1;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,16 @@
 package tudelft.ghappy;
 
 public class GHappyTest {
+    @ParameterizedTest(name = "str={0}")
+    @CsvSource({ "hello", "check", "a", "b" })
+    public void testForNog(){
+        Ghappy no_g = new Ghappy();
+        Assertions.assertFalse(no_g.ghappy(str));
+    }
+
+    @ParameterizedTest(name = "str={0}")
+    @CsvSource({ "gg", "hegghe", "hellogg", "gghello", "ggheggdf"})
+    public void testForNog(){
+        Ghappy no_g = new Ghappy();
+        Assertions.assertTrue(no_g.ghappy(str));
 }
